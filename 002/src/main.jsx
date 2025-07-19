8b60200afd2e440390fe638911a8da3931c73fad
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-19 20:02:56 +0300

    starting to react

--- 002/src/main.jsx
+++ 002/src/main.jsx
@@ -1,3 +1,83 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Mapping Collections to JSX Elements
+
+
+
+
+import ReactDOM from "react-dom/client";
+
+const array = ["React", "is", "awesome"];
+const object = { first: 1, second: 2, third: 3 };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <section>
+    <h1>Array</h1>
+    <ul>
+      {array.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+
+    <h1>Object</h1>
+    <ul>
+      {
+        Object.keys(object).map((key) => (
+          <li key={key}>
+            <strong>{key}: </strong>
+            {object[key]}
+          </li>
+        ))
+      }
+    </ul>
+  </section>
+)
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 import ReactDOM from "react-dom/client";
 
 const handleClick = () => {
@@ -12,3 +92,5 @@ root.render(
     <button onClick={() => alert("Tıklandı!")}>Tıkla</button>
   </section>
 );
+
+*/

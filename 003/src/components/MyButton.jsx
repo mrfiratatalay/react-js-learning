811638d8b673c841b9aa5879c834e9cec47582df
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-24 19:07:20 +0300

    Bir bileşene gelen props değişirse React yalnızca değişen kısmı günceller

--- 003/src/components/MyButton.jsx
+++ 003/src/components/MyButton.jsx
@@ -2,5 +2,30 @@ const MyButton = ({ disabled, text }) => {
     return <button disabled={disabled}>{text}</button>;
 }
 
-
 export default MyButton;
+
+
+{
+    /*
+        const MyButton = ({ disabled, text }) => (
+            <button disabled={disabled}>{text}</button>
+        )
+
+        MyButton.defaultProps = {
+            disabled: false,
+            text: "Click Me"
+        }
+
+    */
+
+
+}
+
+{
+    /*
+    const MyButton = ({disabled = false, text = "My Button"}) => {
+        <button>disabled={disabled}>{text}</button>;
+        }
+
+    */
+}

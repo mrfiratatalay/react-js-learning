e69d98bee0fad1c7230a7b7104f93b937d1ade33
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-28 01:25:06 +0300

    Monolithic Approach and Moduler Yaklasim

--- 006/src/App.jsx
+++ 006/src/App.jsx
@@ -1,35 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const id = (function* () {
+  let i = 1;
+  while (true) {
+    yield i;
+    i += 1;
+  }
+})();
+
+export default function App() {
+  const [articles, setArticles] = useState([
+    {
+      id: id.next().value,
+      title: "Article 1",
+      summary: "Article 1 Summary",
+      display: "none",
+    },
+    {
+      id: id.next().value,
+      title: "Article 2",
+      summary: "Article 2 Summary",
+      display: "none",
+    },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+
+  const onChangeTitle = useCallback((e) => {
+    setTitle(e.target.value);
+  })
+
+  const onChangeSummary = useCallback((e) => {
+    setSummary(e.target.value);
+  })
+
+  const onClickAdd = useCallback(() => {
+    setArticles((state) => [
+      ...state,
+      {
+        id: id.next().value,
+        title: title,
+        summary: summary,
+        display: "none"
+      }
+    ])
+    setTitle("");
+    setSummary("");
+  }, [summary, title]);
+
+  const onClickRemove = useCallback((id) => {
+    setArticles((state) =>
+      state.filter((article) => article.id !== id)
+    )
+  }, [])
+
+
+  const onClickToggle = useCallback((id) => {
+    setArticles((state) => {
+      const articles = [...state];
+      const index = articles.findIndex((article) => article.id === id);
+      articles[index] = {
+        ...articles[index],
+        display: articles[index].display ? "" : "none",
+      };
+      return articles;
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section>
+      <header>
+        <h1>Articles</h1>
+        <input
+          placeholder="Title"
+          value={title}
+          onChange={onChangeTitle}
+        />
+        <input
+          placeholder="Summary"
+          value={summary}
+          onChange={onChangeSummary}
+        />
+        <button onClick={onClickAdd}>Add</button>
+      </header>
+
+      <article>
+        <ul>
+          {
+            articles.map((i) => (
+              <li key={i.id}>
+                <a
+                  href={`#${i.id}`}
+                  title="Toggle Summary"
+                  onClick={() => onClickToggle(i.id)}
+                >{i.title}</a>
+                &nbsp;
+                <button
+                  href={`#${i.id}`}
+                  title="Remove"
+                  onClick={() => onClickRemove(i.id)}
+                >
+                  &#10007;
+                </button>
+                <p style={{ display: i.display }}>{i.summary}</p>
+              </li>
+            ))
+          }
+        </ul>
+      </article>
+    </section>
   )
 }
-
-export default App

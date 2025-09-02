0a6cd6d77edc5810bc72e3429a1e909b24690030
Fırat Atalay <firatatalay.cs@gmail.com>
2025-09-02 05:35:21 +0300

    Handling Navigation with Routes

---
+++ 012/src/MyComponent.tsx
@@ -0,0 +1,5 @@
+function MyComponent() {
+    return <p>Hello Route!</p>
+}
+
+export default MyComponent;
--- 012/src/main.tsx
+++ 012/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+//For Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import MyComponent from './MyComponent';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MyComponent />
+  }
+])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

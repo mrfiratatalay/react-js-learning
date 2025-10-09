f16834d7bab9d740c435381301ed470e181c38a1
Fırat Atalay <firatatalay.cs@gmail.com>
2025-10-09 16:11:02 +0300

    MUI checkboxes-and-radio-buttons

--- 028/src/App.tsx
+++ 028/src/App.tsx
@@ -1,7 +1,32 @@
-import LambaKontrol from './components/LambaControl';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import { useState } from 'react';
 
 function App() {
-  return <LambaKontrol />;
+  const [checkbox, setCheckbox] = useState(false);
+  const [radio, setRadio] = useState('First');
+
+  return (
+    <div>
+      <FormControlLabel
+        label={`Checkbox ${checkbox ? '(checked)' : ''}`}
+        control={<Checkbox checked={checkbox} onChange={() => setCheckbox(!checkbox)} />}
+      />
+
+      <FormControl component="fieldset">
+        <FormLabel component="legend">{radio}</FormLabel>
+        <RadioGroup value={radio} onChange={(e) => setRadio(e.target.value)}>
+          <FormControlLabel value="First" label="First" control={<Radio />} />
+          <FormControlLabel value="Second" label="Second" control={<Radio />} />
+          <FormControlLabel value="Third" label="Third" control={<Radio />} />
+        </RadioGroup>
+      </FormControl>
+    </div>
+  );
 }
 
 export default App;
--- 028/src/components/LambaControl.tsx
+++ 028/src/components/LambaControl.tsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
 import { useState } from 'react';
@@ -19,11 +20,17 @@ function LambaControl() {
     <Box sx={{ padding: 2, border: '1px solid #ccc', borderRadius: '8px' }}>
       <Typography variant="h6">{lambaAcikMi ? 'Lamba Acik' : 'Lamba Kapali'}</Typography>
       <Switch checked={lambaAcikMi} onChange={handleSwitchChange} color="warning" />
+
       <Box sx={{ marginTop: 2 }}>
         <Button variant="contained" onClick={handleButtonClick}>
           Durumu Ters Cevir
         </Button>
       </Box>
+
+      <FormControlLabel
+        control={<Switch checked={lambaAcikMi} onChange={handleSwitchChange} />}
+        label={lambaAcikMi ? 'Lamba Acik' : 'Lamba Kapali'}
+      />
     </Box>
   );
 }

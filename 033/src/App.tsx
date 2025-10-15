73e039d999b4f1785d5213a4db051a827405ace4
Fırat Atalay <firatatalay.cs@gmail.com>
2025-10-15 16:40:49 +0300

    Fetching Data from a Server

--- 033/src/App.tsx
+++ 033/src/App.tsx
@@ -1,9 +1,39 @@
-import DenemeFetchApi from "./DenemeFetchApi"
+import { useEffect, useState } from 'react';
+import type { GithubUser } from './GithubUser';
+import UserInfo from './UserInfo';
+
 function App() {
+  const [user, setUser] = useState<GithubUser>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('https://api.github.com/users/xmrfiratatalay')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not OK');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUser(data);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+        setError(error.message);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <>
-      <DenemeFetchApi />
-    </>
+    <div>
+      {loading && <p>Loading...</p>}
+      {!loading && !user && <div>Hata olustu: {error}</div>}
+      {user && <UserInfo user={user} />}
+    </div>
   );
 }
 export default App;

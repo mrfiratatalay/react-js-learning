a82a925d58b5064edbb3cc550170336a8dcfbf24
Fırat Atalay <firatatalay.cs@gmail.com>
2025-10-16 03:30:17 +0300

    Fetching Data from a Server

--- 035/src/App.tsx
+++ 035/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from '@tanstack/react-query';
+import UserInfo from './UserInfo';
+
+const userFetcher = (username: string) =>
+  fetch(`https://api.github.com/users/${username}`).then((response) => response.json());
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    data: user,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ['githubUser'],
+    queryFn: () => userFetcher('mrfiratatalay'),
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {isPending && <p>Loading...</p>}
+      {isError && <p>Error fetching data</p>}
+      {user && <UserInfo user={user} />}
+    </div>
+  );
 }
 
-export default App
+export default App;

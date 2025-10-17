6f50bcef7439929572a7bed291fc77e101e3b9c6
Fırat Atalay <firatatalay.cs@gmail.com>
2025-10-17 23:56:45 +0300

    Fetching Data from a Server

--- 036/src/App.tsx
+++ 036/src/App.tsx
@@ -1,8 +1,38 @@
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client/react';
+import type { GithubUser } from './GithubUser';
+import UserInfo from './UserInfo';
+
+const GET_GITHUB_USER = gql`
+  query GetGithubUser($username: String!) {
+    user(login: $username) {
+      login
+      id
+      avatarUrl
+      bio
+      name
+      company
+      location
+    }
+  }
+`;
+
 function App() {
+  const { data, loading, error } = useQuery<{ user: GithubUser }>(GET_GITHUB_USER, {
+    variables: { username: 'mrfiratatalay' },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching data</p>;
+
+  if (!data || !data.user) return <p>No user found</p>; // ✅ type-safe guard
+
+  const user = data.user;
+
   return (
-    <>
-      <p>Apollo and Graphql</p>
-    </>
+    <div>
+      <UserInfo user={user} />
+    </div>
   );
 }
 

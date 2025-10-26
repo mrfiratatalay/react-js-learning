bca1bb1bd5550efd97b338bb146123cb6e7b5ba9
Fırat Atalay <firatatalay.cs@gmail.com>
2025-10-26 15:29:14 +0300

    State Management in React

---
+++ 041/src/features/counter/counterSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  count: 0,
+};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+
+  reducers: {
+    //case "INCREMENT"
+    increment: (state) => {
+      state.count += 1;
+    },
+
+    //case "DECREMENT"
+    decrement: (state) => {
+      state.count -= 1;
+    },
+
+    addByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+
+    reset: (state) => {
+      state.count = 0;
+    },
+  },
+});
+
+export const { increment, decrement, addByAmount, reset } = counterSlice;
+export default counterSlice.reducer;
+
